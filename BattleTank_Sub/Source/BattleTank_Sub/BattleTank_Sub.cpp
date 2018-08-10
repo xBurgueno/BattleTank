9e1c1f47da20bf74439e4e0fddd14bd82bd95d58
xBurgueno <xavierburgueno.xb@gmail.com>
2018-08-09 22:09:58 -0700

    BattleTank Project Created + Simple Terrain made

---
+++ BattleTank_Sub/Source/BattleTank_Sub/BattleTank_Sub.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank_Sub.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTank_Sub, "BattleTank_Sub" );
